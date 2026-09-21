efbafcaaa6645c6fd0e2cd4b9ddf4babb9673498
Luisfer0503 <coyotecatlluis10@gmail.com>
2026-09-21 11:38:05 -0600

    fix short_open_tag compatibility on production server for XML feed

--- resources/views/feeds/productos-xml.blade.php
+++ resources/views/feeds/productos-xml.blade.php
@@ -1,4 +1,4 @@
-@php echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n"; @endphp
+{!! '<' . '?xml version="1.0" encoding="UTF-8"?' . '>' !!}
 <rss version="2.0" xmlns:g="http://base.google.com/ns/1.0">
   <channel>
     <title><![CDATA[{{ config('app.name', 'Sector Mueble') }} - Catálogo de Productos]]></title>
