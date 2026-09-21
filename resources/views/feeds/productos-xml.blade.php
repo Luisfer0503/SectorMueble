ee3b9f24a866b7fc94ebd114c4c751f7b2e392bd
Luisfer0503 <coyotecatlluis10@gmail.com>
2026-09-21 11:36:44 -0600

    fix VS Code linter syntax warning on XML header

--- resources/views/feeds/productos-xml.blade.php
+++ resources/views/feeds/productos-xml.blade.php
@@ -1,4 +1,4 @@
-{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
+@php echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n"; @endphp
 <rss version="2.0" xmlns:g="http://base.google.com/ns/1.0">
   <channel>
     <title><![CDATA[{{ config('app.name', 'Sector Mueble') }} - Catálogo de Productos]]></title>
